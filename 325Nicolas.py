769091ef74ac5abe4ad5bdd2198e5666f15fde9a
abd33223 <145920460+abd33223@users.noreply.github.com>
2023-09-24 22:18:12 +0300

    Update 325Nicolas.py

--- 325Nicolas.py
+++ 325Nicolas.py
@@ -6,19 +6,33 @@ import plotly.graph_objects as go
 # Load earthquake data
 df = pd.read_csv("earthquake_data.csv")
 
+# Introductory page content
+st.title("Earthquakes")
+st.header("MSBA 325")
+st.subheader("Nicolas Araman")
+
+# Paragraph about high magnitude earthquakes
+st.write(
+    """
+    High magnitude earthquakes can cause significant devastation, including widespread damage to buildings, 
+    infrastructure, and loss of lives. The intensity and impact of these earthquakes highlight the urgent need 
+    for preparedness, monitoring, and proactive measures to mitigate potential damage and save lives.
+    """
+)
+
 # Plotting earthquake locations
-st.write("Earthquake Locations")
+st.write("## Earthquake Locations")
 fig_map = px.scatter_geo(df, lat='latitude', lon='longitude', color='magnitude', hover_name='location',
                          title='Earthquake Locations')
 st.plotly_chart(fig_map)
 
 # Magnitude Distribution Over Time (Animated Histogram)
-st.write("Magnitude Distribution Over Time")
+st.write("## Magnitude Distribution Over Time")
 fig_histogram = px.histogram(df, x='magnitude', nbins=30, title='Magnitude Distribution Over Time')
 st.plotly_chart(fig_histogram)
 
 # 3D Scatter Plot
-st.write("3D Scatter Plot of Earthquake Data (Depth, Magnitude, Gap)")
+st.write("## 3D Scatter Plot of Earthquake Data (Depth, Magnitude, Gap)")
 fig_3d_scatter = px.scatter_3d(df, x='depth', y='magnitude', z='gap', color='country',
                                 size='nst', opacity=0.7, hover_name='title',
                                 labels={'depth': 'Depth', 'magnitude': 'Magnitude', 'gap': 'Gap',
@@ -27,7 +41,7 @@ fig_3d_scatter = px.scatter_3d(df, x='depth', y='magnitude', z='gap', color='cou
 st.plotly_chart(fig_3d_scatter)
 
 # Earthquake Density Contours by Magnitude
-st.write("Earthquake Density Contours by Magnitude")
+st.write("## Earthquake Density Contours by Magnitude")
 fig_density_contour = px.density_contour(df, x='longitude', y='latitude', color='magnitude',
                                         marginal_x='histogram', marginal_y='histogram',
                                         labels={'longitude': 'Longitude', 'latitude': 'Latitude', 'magnitude': 'Magnitude'},
@@ -35,7 +49,7 @@ fig_density_contour = px.density_contour(df, x='longitude', y='latitude', color=
 st.plotly_chart(fig_density_contour)
 
 # Cumulative Earthquake Count Over Time (Animated Line Chart)
-st.write("Cumulative Earthquake Count Over Time")
+st.write("## Cumulative Earthquake Count Over Time")
 df['date_time'] = pd.to_datetime(df['date_time'])
 df.sort_values(by='date_time', inplace=True)
 df['cumulative_count'] = range(1, len(df) + 1)
